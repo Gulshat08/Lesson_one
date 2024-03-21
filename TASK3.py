2202509034f350c1522fb97be6c4aa64b4daded3
Gulshat08 <161115561+Gulshat08@users.noreply.github.com>
2024-03-21 21:23:14 +0400

    Add files via upload

---
+++ TASK3.py
@@ -0,0 +1,6 @@
+x1=int(input('variable value x1:'))
+x2=int(input('variable value x2:'))
+x3=int(input('variable value x3:'))
+operation = str(input('sign'))
+if operation =='/':
+    print(x1/x2*x3)
